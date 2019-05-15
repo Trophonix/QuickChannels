b7c303f2c513ee4cb244b95b152ff852a35a3381
Lucas <502badgamer@gmail.com>
2019-05-15 16:27:01 -0500

    v1.11

--- src/main/java/com/trophonix/quickchannels/QuickChannels.java
+++ src/main/java/com/trophonix/quickchannels/QuickChannels.java
@@ -7,6 +7,7 @@ import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.AsyncPlayerChatEvent;
 import org.bukkit.event.player.PlayerQuitEvent;
@@ -39,12 +40,19 @@ public class QuickChannels extends JavaPlugin implements Listener {
     private ConfigMessage error;
     private ConfigMessage reloaded;
     private ConfigMessage noLinks;
+    private ConfigMessage forcedIn;
+    private ConfigMessage forcedOut;
+    private ConfigMessage playerNotFound;
+    private ConfigMessage forcedSuccess;
+    private ConfigMessage forcedNoChannel;
 
     private Map<UUID, String> channels = new HashMap<>();
 
     @Override
     public void onEnable() {
-        saveDefaultConfig();
+        getConfig().options().copyDefaults(true);
+        saveConfig();
+
         reloadMessages();
 
         getServer().getPluginManager().registerEvents(this, this);
@@ -65,40 +73,51 @@ public class QuickChannels extends JavaPlugin implements Listener {
                 sender.sendMessage(ChatColor.RED + "You must be a player to do that.");
                 return true;
             }
-            Player player = (Player)sender;
+            Player player = (Player) sender;
             if (args[0].equalsIgnoreCase("leave")) {
                 String channel = getChannel(player);
                 if (channel == null) {
                     error.send(this, sender);
                     return true;
                 }
-                setChannel(player, null);
-                leave.send(this, player, "{channel}", channel);
-                left.send(this, getChannelMembers(channel), "{channel}", channel, "{player}", player.getName(), "{members}", getChannelMembersString(channel));
-                String sound = getConfig().getString("sounds.leave", "villager_no");
-                Sounds.play(player, sound);
-                playSoundToChannel(channel, sound);
+                setChannel(player, null, false);
+                return true;
+            } else if (args.length >= 2 && args[0].equalsIgnoreCase("force") && sender.hasPermission("quickchannels.admin")) {
+                Player target = Bukkit.getPlayer(args[1]);
+                if (target == null) {
+                    playerNotFound.send(this, sender, "{player}", args[1]);
+                    return true;
+                }
+                if (args.length == 2) {
+                    if (getChannel(target) == null) {
+                        forcedNoChannel.send(this, sender, "{player}", target.getName());
+                        return true;
+                    }
+                    setChannel(target, null, true);
+                } else {
+                    String channel = args[2].toLowerCase();
+                    setChannel(target, channel, true);
+                }
+                forcedSuccess.send(this, sender, "{player}", target.getName());
                 return true;
             }
             String current = getChannel(player);
             if (current != null) Bukkit.dispatchCommand(player, "channel leave " + current);
             String channel = args[0].toLowerCase();
-            setChannel(player, channel);
-            String sound = getConfig().getString("sounds.join", "pling");
-            playSoundToChannel(channel, sound);
+            setChannel(player, channel, false);
             return true;
         }
         help.send(this, sender);
         return true;
     }
 
-    @EventHandler
+    @EventHandler (priority = EventPriority.LOW)
     public void onChat(AsyncPlayerChatEvent event) {
         Player player = event.getPlayer();
         String msg = event.getMessage();
         if (msg.trim().equals(prefix)) return;
-        if (msg.startsWith(prefix) || !prefixSendsToChannel) {
-            msg = msg.substring(prefix.length());
+        if (prefixSendsToChannel == msg.startsWith(prefix)) {
+            if (prefixSendsToChannel) msg = msg.substring(prefix.length());
             String channel = getChannel(player);
             if (channel != null) {
                 event.setCancelled(true);
@@ -122,28 +141,40 @@ public class QuickChannels extends JavaPlugin implements Listener {
                 String sound = getConfig().getString("sounds.message", "click");
                 playSoundToChannel(channel, sound);
             } else if (prefixSendsToChannel) {
-                event.setCancelled(true);
                 error.send(this, player);
+                event.setCancelled(true);
             }
+        } else if (!prefixSendsToChannel && msg.startsWith(prefix)) {
+            event.setMessage(msg.substring(prefix.length()));
         }
     }
 
     @EventHandler
     public void onQuit(PlayerQuitEvent event) {
-        if (removeOnQuit) this.channels.remove(event.getPlayer().getUniqueId());
+        if (removeOnQuit) setChannel(event.getPlayer(), null, false);
     }
 
     private String getChannel(@NotNull Player player) {
         return channels.get(player.getUniqueId());
     }
 
-    private void setChannel(@NotNull Player player, String channel) {
+    private void setChannel(@NotNull Player player, String channel, boolean forced) {
         if (channel == null) {
+            String previousChannel = getChannel(player);
             channels.remove(player.getUniqueId());
+            if (forced) forcedOut.send(this, player, "{channel}", previousChannel, "{player}", player.getName());
+            else leave.send(this, player, "{channel}", previousChannel);
+            left.send(this, getChannelMembers(previousChannel), "{channel}", previousChannel, "{player}", player.getName(), "{members}", getChannelMembersString(previousChannel));
+            String sound = getConfig().getString("sounds.leave", "villager_no");
+            playSoundToChannel(previousChannel, sound);
         } else {
             channels.put(player.getUniqueId(), channel);
-            join.send(this, player, "{channel}", channel, "{player}", player.getName(), "{members}", getChannelMembersString(channel));
-            joined.send(this, getChannelMembers(channel, player), "{channel}", channel, "{player}", player.getName(), "{members}", getChannelMembersString(channel));
+            String memberString = getChannelMembersString(channel);
+            if (forced) forcedIn.send(this, player, "{channel}", channel, "{player}", player.getName(), "{members}", memberString);
+            else join.send(this, player, "{channel}", channel, "{player}", player.getName(), "{members}", memberString);
+            joined.send(this, getChannelMembers(channel, player), "{channel}", channel, "{player}", player.getName(), "{members}", memberString);
+            String sound = getConfig().getString("sounds.join", "pling");
+            playSoundToChannel(channel, sound);
         }
     }
 
@@ -194,6 +225,11 @@ public class QuickChannels extends JavaPlugin implements Listener {
         this.error = new ConfigMessage(getConfig(), "messages.error");
         this.reloaded = new ConfigMessage(getConfig(), "messages.reloaded");
         this.noLinks = new ConfigMessage(getConfig(), "messages.no-links");
+        this.forcedIn = new ConfigMessage(getConfig(), "messages.forced-in");
+        this.forcedOut = new ConfigMessage(getConfig(), "messages.forced-out");
+        this.playerNotFound = new ConfigMessage(getConfig(), "messages.player-not-found");
+        this.forcedSuccess = new ConfigMessage(getConfig(), "messages.forced-success");
+        this.forcedNoChannel = new ConfigMessage(getConfig(), "messages.forced-no-channel");
     }
 
     public String getPrefix() {
